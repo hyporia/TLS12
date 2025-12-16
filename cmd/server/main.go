c8fc438a0165a0cd440c632e3212b2f0503e7fc0
hyporia <maximgalimov96@gmail.com>
2025-12-16 17:16:15 +0100

    * apply security fix for network interface

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,9 +1,11 @@
 package main
 
-import "net"
+import (
+	"net"
+)
 
 func main() { // coverage-ignore
-	listener, err := net.Listen("tcp", ":6969")
+	listener, err := net.Listen("tcp", "127.0.0.1:6969")
 
 	if err != nil {
 		panic(err)
